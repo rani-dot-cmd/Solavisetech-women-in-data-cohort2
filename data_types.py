e9370514ae6fde99e790081cd62d03d447a2a98b
Vanessa Liisone <ranillaliisoni@gmail.com>
2026-05-21 08:46:10 +0100

    Create personal bio generator in data_types.py
    Added a personal bio generator with basic information.

---
+++ data_types.py
@@ -0,0 +1,13 @@
+# Personal Bio Generator
+
+name = "Ranilla"
+age = 22
+height = 1.65
+favorite_tech_field = "Data Science"
+is_student = True
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My height is", height, "meters")
+print("My favorite tech field is", favorite_tech_field)
+print("Student Status:", is_student)
